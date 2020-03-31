3d128bdf88cf3178006ec9d1adfd4e439ca187fb
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-03-30 19:09:17 -0600

    using SERVER

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 
 <html>
 <header><title>BONE HAUS-CONTACT </title></header>
-<link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 
-<h1>BONE HAUS</h1>
+    <h1>BONE HAUS</h1>
        
        <h2> 
         <a href="index.html" id="Shows" >Shows</a>
@@ -17,17 +17,15 @@
           <a href="https://www.instagram.com/bonehausofficial/"> Instagram</a> or at hmiller10191@gmail.com</p></div>
 
     <body>
-      
-            <form action="" method="POST">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
              <p> Name: <input type="text" name="name" value=""></p>
              <p> E-mail: <input type="text" name="email" value =""></p>
               <p>Message: <textarea name="message" rows="5" cols="40"> </textarea></p>
               <p><input type="submit" name="submit" value="Submit"></p>
             </form>
 
-            <div id="output" name="output" align="center">
+      <div id="output" name="output" align="center">
           <?php 
-          
           if (isset($_POST['submit'])) {
             echo "Thanks you for contacting us ".$_POST['name']."!".'<br />';
             echo "We will email you at ".$_POST['email'].'<br />';
