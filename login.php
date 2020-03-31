fe807ff3485cc68100328cf68a117d2d9a6ad17c
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-03-30 20:51:22 -0600

    Session time

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    ?>
+
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+            <h1>Please Log In</h1>
+            <?php
+                if (isset($SESSION['message'])){
+                    echo "<div id='error'>{$_SESSION['message']}</div>";
+                    unset($_SESSION['message']);
+                }
+            ?>
+
+            <form action="login_handler.php" method="POST">
+                <div><label for="username">Username: <input type="textbox" id="username" name="username" /></div>
+                <div><label for="password">Password: <input type="password" id="password" name="password" /></div>
+                <div><input type="submit"/></div>
+            </form>
+        </body>
+    </html>
---
+++ login_handler.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$username = "andremaldonado";
+$password = "Boise123!";
+
+if($username == $_POST['username'] && $password == $_POST['password']){
+    $_SESSION['auth'] = true;
+    header("Location: http://CS401/message.php")
+    exit;
+} else{
+    $_SESSION['auth'] = false;
+    $_SESSION['message'] = "Invalid username or password";
+    header("Location: http://CS401/login.php");
+}
