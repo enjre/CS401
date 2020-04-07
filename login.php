0bc918098625b72a673b32055fd77abaeebb8586
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-06 18:12:26 -0700

    patching login

--- login.php
+++ login.php
@@ -16,9 +16,33 @@
             ?>
 
             <form action="login_handler.php" method="POST" align="center">
-                <div><label for="username">Username: <input type="textbox" id="username" name="username" /></div>
-                <div><label for="password">Password: <input type="password" id="password" name="password" /></div>
-                <div><input type="submit"/></div>
+                <label> Username</label>
+                <input type="text" name="username">
+                <label>Password</label>
+                <input type="text" name="pass">
+                <input type="submit" name="submit" value="submit">
             </form>
         </body>
     </html>
+
+    <?php 
+        $conn = mysqli_connect("localhost","root", "", "admin"); 
+
+        if(isset($_POST["submit"])){
+            $username = $_POST["username"];
+            $pass = $_POST["pass"];
+
+            $sql = mysqli_query($conn, "SELECT count(*) as total from Login where username = '".$username."' and password = '".$pass."'") or die(mysqli_error($conn));
+        
+            $rw = mysqli_fetch_array($sql);
+
+            if($rw['total'] > 0) {
+                echo "<script>alert('username and password are correct'</script>";
+            } else{
+                echo "<script>alert('username and password are incorrect'</script>";
+
+            }
+
+
+        }
+    ?>
