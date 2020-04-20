ccfd838790b84e2cf7f06a5911ede5713db9e356
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 17:10:06 -0600

    removed icon

--- navbar.php
+++ navbar.php
@@ -1,6 +1,4 @@
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome.min.css">
-<head>
+
 
 <div id="right" class="navbar">
 <ul>
@@ -16,7 +14,7 @@
 <div id="left" class="navbar">
     <ul>
         <li <?php if ($thisPage == "index")
-            echo " id=\"currentpage\""; ?> ><a href="index.php"> <i class="fa fa-fw fa-home"></i>SHOWS</a></li>
+            echo " id=\"currentpage\""; ?> ><a href="index.php">SHOWS</a></li>
 
         <li <?php if ($thisPage == "media")
             echo " id=\"currentpage\""; ?> ><a href="media.php">MEDIA</a></li>
