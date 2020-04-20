9950091f0302eee3dba51a75f759565030eca0ed
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 02:58:49 -0600

    center login

--- login.php
+++ login.php
@@ -14,25 +14,26 @@
         <h1>BONE HAUS</h1>
       </div>
       
-      <h2>
+        <h2>
             <?php include("navbar.php"); ?>
         </h2>
     
-
+        <div class="loginbox">
             <?php 
-        if(isset($_SESSION['message'])) {
-             echo "<div id='error'>{$_SESSION['message']}</div>";
-        }
-            unset($_SESSION['message']);
-            
-
-    ?>
+                if(isset($_SESSION['message'])) {
+                    echo "<div id='error'>{$_SESSION['message']}</div>";
+                }
+                    unset($_SESSION['message']);
+             ?>
+        
 
             <form action="login_handler.php" method="post">
                 <div><label for="username"> Username:<input type="text" id="username" name="username" /></div>
                 <div><label for="password">Password:<input type="password" id="password" name="password" /></div>
                 <div><input type="submit" name="submit" value="submit"></div>
             </form>
+
+            </div>
         </body>
     </html>
 
