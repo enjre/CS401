6b06a756db3ed1a07ee73d3186553e58b5179fad
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 11:41:01 -0600

    second gallery col

--- gallery.php
+++ gallery.php
@@ -18,17 +18,23 @@
         
           <div class="col">
               <div class="box"><img src="imgs/bh1.jpg" width="500px" height="500px"></div>
-              <div class="box"><img src="imgs/bh2.jpg"></div>
+              <!-- <div class="box"><img src="imgs/bh2.jpg"></div> -->
               <div class="box"><img src="imgs/bh3.jpg" width="500px" height="500px"></div>
               <div class="box"><img src="imgs/bh4.jpg"></div>
               <div class="box"><img src="imgs/bh5.jpg" width="800px" height="600px"></div>
               <div class="box"><img src="imgs/bh6.jpg" height="500px"></div>
+              
+          </div>
+
+          <div class="col">
               <div class="box"><img src="imgs/bh7.jpg"></div>
               <div class="box"><img src="imgs/bh8.jpg"></div>
               <div class="box"><img src="imgs/bh9.jpg"></div>
               <div class="box"><img src="imgs/bh10.jpg"></div>
               <div class="box"><img src="imgs/bh11.jpg" width="400px" height="600px"></div>
+
           </div>
+
           
             <div class="full-image">
               <img class="sticky" src="imgs/bh1.jpg" width="500px" height="500px">
