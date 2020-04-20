0bdb594d0d1bed29a7ee9d1042f2e5ecc044f7ea
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 01:25:01 -0600

    changes to login_handler

--- login_handler.php
+++ login_handler.php
@@ -1,37 +1,77 @@
 <?php
 
 session_start();
+require_once " Dao.php";
 
-//$username = "andremaldonado";
-//$password = "Boise123!";
 
-// function checkInput ($dat){
-//     $dat = trim($dat);
-//     $dat = stripslashes($dat);
-//     $dat = htmlspecialchars($dat);
-//     return $dat;
-// }
+$dao = new Dao();
+$email = $_REQUEST['email'];
+$email = filter_var($email,FILTER_SANITIZE_EMAIL);
+$password = $_REQUEST['password'];
+$user = $dao->userLogin($username);
+$_SESSION['email_preset'] = $_POST['email'];
+$valid = true;
+$messages = array();
 
-//  $username= checkInput($_POST['username']);
-//  $password= checkInput($_POST['password']);
 
-$username = 'andremaldonado';
-$password = 'Boise123!';
+
 
 $salt = 'abs0lutez3r0';
 $saltedPW = $password . $salt;
 $hashedPW = hash('sha256', $saltedPW);
 
-if(empty($username)) {
-    $_SESSION['good'] = false;
-    $_SESSION['message'] = "Username cannot be empty";
-    header("Location: login.php");
-    exit();
+
+if((empty($username)) && (empty($password))) {
+    $messages[] = "Username and password cannot be blank";
+    $valid = false;
+    echo " Please enter a valid username";
 }
 
+if(empty($username)){
+    $messages[] = "Please enter a valid username";
+    $valid = false;
+    echo "Please enter a valid username";
+}
 
-require_once 'Dao.php';
-$dao = new Dao();
+if(empty($password)){
+    $messages[] = "Please enter a valid password";
+    $valid = false;
+    echo "Please enter a valid password";
+}
+
+if(!$user) {
+    $messages[] = "Username does not exist";
+    $valid = false;
+} else {
+    $user = $dao->getUser($username);
+    $pass = $dao->getPassword($password);
+    $passhash = hash("sha256", $password . "z3r0");
+
+
+    if($passhash != $pass) {
+        $messages[] = "Incorrect password";
+        echo " invalid password";
+        $valid = false;
+        $_SESSION["user_preset"] = $username;
+    } else {
+        $messages[] = "Welcome $username";
+        $_SESSION['currentUser'] = $username;
+        $valid = true;
+    }
+}
+
+if(!$valid) {
+    $_SESSION['sentiment'] = "Incorrect email or password";
+    $_SESSION['messages'] = $messages;
+        header("Location: login.php");
+        exit();
+}
+
+$_SESSION['sentiment'] = "good";
+header("Location: index.php");
+?>
+
+
 
 $loginMatch = $dao->userLogin($username, $hashedPW);
 
