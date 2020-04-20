5ffcad3112ebd463d59c70123fddef5bb6f2bfa5
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 10:35:02 -0600

    removed slick

--- gallery.php
+++ gallery.php
@@ -3,8 +3,6 @@
 
   <head>
 
-    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
 <header><title>BONE HAUS-GALLERY </title></header>
@@ -36,36 +34,24 @@
               <div class="box"><img src="imgs/bh10.jpg"></div>
               <div class="box"><img src="imgs/bh11.jpg" width="400px" height="600px"></div>
           </div>
-          <!-- <div class="col">
+          <div class="col">
             <div class="full-image">
               <img src="imgs/bh1.jpg" width="500px" height="500px">
             </div>
 
-          </div> -->
+        </div>
         </div>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script> 
+        
 
-<!-- <script type="text/javascript">
+<script type="text/javascript">
   $(document).ready(function(){
     $('.box').click(function(){
       $('.full-image').html($(this).html());
     });
   });
 });
-</script> -->
-
-<script type="text/javascript">
- $(document).ready(function(){
-  $('.col').slick({
-    setting-name: setting-value
-  });
 </script>
-
-
 
 
 
