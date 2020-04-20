8c12cb84a49093f4efba07b7752e7a3f7498c853
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 02:55:10 -0600

    added navbar to sign in page

--- login.php
+++ login.php
@@ -14,12 +14,8 @@
         <h1>BONE HAUS</h1>
       </div>
       
-        <h2>
-            <a href="index.php" id="Shows" >Shows</a>
-            <a href="media.html" id="Media" >Media</a>
-            <a href="gallery.html" id="Gallery" >Gallery</a>
-            <a href="contact.php" id="Contact" >Contact</a>
-            <a href="login.php" id="login">Login</a>
+      <h2>
+            <?php include("navbar.php"); ?>
         </h2>
     
 
