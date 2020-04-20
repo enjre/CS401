52b240d40fd40de424aad0fbf767bd35ee736c45
Andre Maldonado <47874633+enjre@users.noreply.github.com>
2020-04-20 15:09:58 -0600

    login_handler changes more

--- login_handler.php
+++ login_handler.php
@@ -14,58 +14,49 @@ $username = filter_var($username,FILTER_SANITIZE_STRING);
 $_SESSION['user'] = $username;
 $_SESSION['pass'] = $password;
 
+if(empty($username) || empty($password))
+	{
+		$_SESSION['errorMessage'] = "Missing in form fields.";
+		$_SESSION['errorEmail'] = "Please Enter a valid Email";
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$_SESSION['errorEmail'] = "Enter a valid Email.";
+		}
+		else
+		{
+			$_SESSION['errorEmail'] = "";
+		}
+		$_SESSION['errorLPass'] = "Please Enter A Password";
+		header("Location: login.php");
+	}
+	else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($password))
+	{
+		$_SESSION['errorEmail'] = "Enter a valid Email.";
+		$_SESSION['errorLPass'] = "";
+		header("Location: login.php");
+	}
+	else
+	{
+		$salt = "eftisfun";
+		$epassword = hash('sha256', $password . $salt);
+		$loginInfo = $dao->checkLogin($email, $epassword);
+	
+		if ($loginInfo == 1) {
+			$_SESSION['errorMessage'] = "";
+			$_SESSION['errorLPass'] = "";
+			$_SESSION['errorEmail'] = "";
+			$_SESSION['currentUser'] = $email;
+			$_SESSION['admin'] = $dao->checkAdmin($email);
+			header("Location: index.php");
+		} 
+		else {
+			$_SESSION['errorMessage'] = "Login Unsuccessful, email or password incorrect.";
+			$_SESSION['errorLPass'] = "";
+			$_SESSION['errorEmail'] = "";
+			header("Location: login.php");
+		}
+	}
 
 
-
-if((empty($username)) || (empty($password))) {
-    
-    $valid = false;
-    echo " Please enter a valid username";
-}
-
-if(empty($username)){
-    $valid = false;
-    echo "Please enter a valid username";
-}
-
-if(empty($password)){
-    $valid = false;
-    echo "Please enter a valid password";
-}
-
-if(!$user) {
-    $messages[] = "Username does not exist";
-    $valid = false;
-} else {
-    $user = $dao->getUser($username);
-    $pass = $dao->getPassword($password);
-    $passhash = hash("sha256", $password . "z3r0");
-
-
-    if($passhash != $pass) {
-        $messages[] = "Incorrect password";
-        echo " invalid password";
-        $valid = false;
-        $_SESSION["user_preset"] = $username;
-    } else {
-        $messages[] = "Welcome $username";
-        $_SESSION['currentUser'] = $username;
-        $valid = true;
-    }
-}
-
-if(!$valid) {
-    $_SESSION['sentiment'] = "Incorrect username or password";
-    $_SESSION['messages'] = $messages;
-        header("Location: login.php");
-        exit();
-}
-
-$salt = 'abs0lutez3r0';
-$saltedPW = $password . $salt;
-$hashedPW = hash('sha256', $saltedPW);
-
-$_SESSION['sentiment'] = "good";
-header("Location: index.php");
 
 ?>
